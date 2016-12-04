2bca487c098fffc94219fdc10f0a6c6405b4c1a2
Liza Kobrazova <lizakobrazova@gmail.com>
2016-12-04 12:50:53 +0300

    Check code for stylistic errors

--- js/assets.js
+++ js/assets.js
@@ -1,33 +1,33 @@
 'use strict';
 
 class Assets {
-    constructor(images, callback) {
-        this.queue = images;
-        this.images = [];
-        this.callback = callback;
-        this.groups = [];
-    }
+  constructor(images, callback) {
+    this.queue = images;
+    this.images = [];
+    this.callback = callback;
+    this.groups = [];
+  }
 
-    loadImages(callback) {
-        if (this.queue.length > 0) {
-            if (callback === undefined) {
-                callback = this.callback;
-            }
-            let img = new Image(),
-                src = this.queue.pop();
+  loadImages(callback) {
+    if (this.queue.length > 0) {
+      if (callback === undefined) {
+        callback = this.callback;
+      }
+      let img = new Image();
+      let src = this.queue.pop();
 
-            let name = src.slice(src.lastIndexOf('/') + 1);
-            img.src = src;
-            img.onload = (function() {
-                this.loadImages(callback);
-            }).bind(this);
-            this.images[name] = img;
-        } else {
-            callback();
-        }
+      let name = src.slice(src.lastIndexOf('/') + 1);
+      img.src = src;
+      img.onload = (function() {
+        this.loadImages(callback);
+      }).bind(this);
+      this.images[name] = img;
+    } else {
+      callback();
     }
+  }
 
-    getImage(img) {
-        return this.images[img];
-    }
+  getImage(img) {
+    return this.images[img];
+  }
 }
--- js/starfield.js
+++ js/starfield.js
@@ -1,48 +1,48 @@
 'use strict';
 
 class Starfield {
-    constructor(game) {
-        this.game = game;
-        this.width = this.game.width;
-        this.height = this.game.height;
-        this.speed = 60;
-        this.density = 200;
-        this.stars = [];
+  constructor(game) {
+    this.game = game;
+    this.width = this.game.width;
+    this.height = this.game.height;
+    this.speed = 60;
+    this.density = 200;
+    this.stars = [];
 
-        for (let i = 0; i < this.density; i++) {
-            this.stars[i] = new Star(Math.random() * this.width, Math.random() * this.height, Math.random() * 3 + 1);
-        }
+    for (let i = 0; i < this.density; i++) {
+      this.stars[i] = new Star(Math.random() * this.width, Math.random() * this.height, Math.random() * 3 + 1);
+    }
+  }
+
+  update(delta) {
+    if (isNaN(delta) || delta <= 0) {
+      return;
     }
 
-    update(delta) {
-        if (isNaN(delta) || delta <= 0) {
-            return;
-        }
-
-        for (let i = 0; i < this.density; i++) {
-            let star = this.stars[i];
-            star.y += delta * this.speed;
-            //	If the star has moved from the bottom of the screen, spawn it at the top.
-            if (star.y > this.height) {
-                this.stars[i] = new Star(Math.random() * this.width, 0, Math.random() * 3 + 1);
-            }
-        }
+    for (let i = 0; i < this.density; i++) {
+      let star = this.stars[i];
+      star.y += delta * this.speed;
+      //	If the star has moved from the bottom of the screen, spawn it at the top.
+      if (star.y > this.height) {
+        this.stars[i] = new Star(Math.random() * this.width, 0, Math.random() * 3 + 1);
+      }
     }
+  }
 
-    render(ctx) {
-        for (let i = 0; i < this.density; i++) {
-            let star = this.stars[i];
-            ctx.beginPath();
-            ctx.arc(star.x, star.y, star.size / 2, 0, 2 * Math.PI);
-            ctx.fillStyle = '#fff';
-            ctx.fill();
-            ctx.closePath();
-        }
+  render(ctx) {
+    for (let i = 0; i < this.density; i++) {
+      let star = this.stars[i];
+      ctx.beginPath();
+      ctx.arc(star.x, star.y, star.size / 2, 0, 2 * Math.PI);
+      ctx.fillStyle = '#fff';
+      ctx.fill();
+      ctx.closePath();
     }
+  }
 }
 
 function Star(x, y, size) {
-    this.x = x;
-    this.y = y;
-    this.size = size;
+  this.x = x;
+  this.y = y;
+  this.size = size;
 }
