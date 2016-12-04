2866223da35a497d9b5000d2f487bd573eab479d
Liza Kobrazova <lizakobrazova@gmail.com>
2016-12-04 12:53:21 +0300

    Change start object methods

--- js/main.js
+++ js/main.js
@@ -3,33 +3,33 @@
 // requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
 
 (function() {
-    let lastTime = 0;
-    let vendors = ['ms', 'moz', 'webkit', 'o'];
-    for (let x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||
-            window[vendors[x] + 'CancelRequestAnimationFrame'];
-    }
+  let lastTime = 0;
+  let vendors = ['ms', 'moz', 'webkit', 'o'];
+  for (let x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||
+      window[vendors[x] + 'CancelRequestAnimationFrame'];
+  }
 
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
-            let currTime = new Date().getTime();
-            let timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            let id = window.setTimeout(function() {
-                    callback(currTime + timeToCall);
-                },
-                timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
+  if (!window.requestAnimationFrame)
+    window.requestAnimationFrame = function(callback, element) {
+      let currTime = new Date().getTime();
+      let timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      let id = window.setTimeout(function() {
+          callback(currTime + timeToCall);
+        },
+        timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
 
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
+  if (!window.cancelAnimationFrame)
+    window.cancelAnimationFrame = function(id) {
+      clearTimeout(id);
+    };
 }());
 
-document.addEventListener('DOMContentLoaded', starPage);
+document.addEventListener('DOMContentLoaded', start.init());
 
 // init new assets object and call main function only after all images loading
 const assets = new Assets([
@@ -37,14 +37,8 @@ const assets = new Assets([
   'img/cookie.png'
 ], main);
 
-function starPage() {
-    const start = new Start();
-    // scroll page to the game menu
-    start.scrollPage();
-    start.setMusic();
-}
 // Main game function.
 function main() {
-    const game = new Game(assets);
-    game.init();
+  const game = new Game(assets);
+  game.init();
 }
--- js/start.js
+++ js/start.js
@@ -1,64 +1,68 @@
 'use strict';
 
-class Start {
-    constructor() {
-        this.playBtn = document.querySelector('.btn-play');
-        this.backBtn = document.querySelector('.btn--back');
-        this.infoBtn = document.querySelector('.btn-info');
-        this.modalPlayBtn = document.querySelector('.warn__btn');
+let start = {
+  init() {
+    addClick('.btn-play', this.startLevel);
+    addClick('.btn--back', this.scrollPage);
+    addClick('.btn-info', this.infoModal);
+    addClick('.warn__btn', this.scrollPage);
 
-        this.playBtn.addEventListener('click', this.startLevel);
-        this.backBtn.addEventListener('click', this.scrollPage);
-        this.infoBtn.addEventListener('click', this.infoModal);
-        this.modalPlayBtn.addEventListener('click', this.scrollPage);
+    function addClick(selector, func) {
+      let elem = document.querySelector(selector);
+      elem.addEventListener('click', func);
     }
 
-    scrollPage() {
-        window.scrollTo(0, document.body.scrollHeight);
-        document.body.style.overflow = 'hidden';
+    // scroll page to the game menu and play music
+    this.scrollPage();
+    this.setMusic();
+  },
+
+  scrollPage() {
+    window.scrollTo(0, document.body.scrollHeight);
+  },
+
+  startLevel() {
+    let lvl = document.querySelector('.game-level--first');
+    let timeCounter = document.querySelector('.game-level__start-count');
+    let timer = 2;
+    let timerId = setInterval(function() {
+      if (timer < 0) {
+        clearInterval(timerId);
+        timeCounter.parentElement.style.display = 'none';
+        assets.loadImages();
+      }
+      timeCounter.innerHTML = timer;
+      timer--;
+    }, 700);
+    window.scrollTo(0, lvl.getBoundingClientRect().top + pageYOffset);
+    document.querySelector('.game-overlay').classList.remove('overlay--active');
+  },
+
+  setMusic() {
+    let musicBtns = document.querySelectorAll('.game__btn-music');
+    for (let i = 0; i < musicBtns.length; i++) {
+      document.querySelector('.game-music').volume = 0.3;
+      musicBtns[i].addEventListener('click', checkMusic);
     }
 
-    startLevel() {
-        let lvl = document.querySelector('.game-level--first'),
-            timeCounter = document.querySelector('.game-level__start-count'),
-            timer = 2,
-            timerId = setInterval(function() {
-                if (timer < 0) {
-                    clearInterval(timerId);
-                    timeCounter.parentElement.style.display = 'none';
-                    assets.loadImages();
-                }
-                timeCounter.innerHTML = timer;
-                timer--;
-            }, 700);
-        window.scrollTo(0, lvl.getBoundingClientRect().top + pageYOffset);
+    function checkMusic() {
+      let audio = document.querySelector('.game-music');
+      this.classList.toggle('btn--musicoff');
+      this.classList.toggle('btn--musicon');
+      if (this.classList.contains('btn--musicon')) {
+        audio.pause();
+      } else {
+        audio.play();
+      }
     }
+  },
 
-    setMusic() {
-        let musicBtns = document.querySelectorAll('.game__btn-music');
-        for (let i = 0; i < musicBtns.length; i++) {
-            document.querySelector('.game-music').volume = 0.3;
-            musicBtns[i].addEventListener('click', checkMusic);
-        }
+  infoModal() {
+    let info = document.querySelector('.game-intro__info');
+    info.classList.add('info--open');
+    document.querySelector('.info-content__btn-close').addEventListener('click', () => {
+      info.classList.remove('info--open');
+    });
+  }
 
-        function checkMusic() {
-            let audio = document.querySelector('.game-music');
-            this.classList.toggle('btn--musicoff');
-            this.classList.toggle('btn--musicon');
-            if (this.classList.contains('btn--musicon')) {
-                audio.pause();
-            } else {
-                audio.play();
-            }
-        }
-    }
-
-    infoModal() {
-      let info = document.querySelector('.game-intro__info');
-      info.classList.add('info--open');
-      document.querySelector('.info-content__btn-close').addEventListener('click', () => {
-        info.classList.remove('info--open');
-      })
-    }
-
-}
+};
