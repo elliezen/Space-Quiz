a1b8073e5d1b110edd903797adb0ada6d5132d67
Liza Kobrazova <lizakobrazova@gmail.com>
2017-11-30 10:50:30 +0300

    Allow debugger in development

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   'extends': 'standard',
   rules: {
     'semi': 0,
-    'space-before-function-paren': 0
+    'space-before-function-paren': 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 };
