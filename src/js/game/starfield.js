4a1d4dc3f720ffae0a7b9dffe2f76f9d629833b0
Liza Kobrazova <lizakobrazova@gmail.com>
2017-11-30 10:54:04 +0300

    Fix star field size

--- src/js/game/starfield.js
+++ src/js/game/starfield.js
@@ -8,23 +8,23 @@ export function init(canvasSize) {
 
   for (let i = 0; i < density; i++) {
     const starData = {
-      x: Math.random() * canvasSize.width - canvasSize.width / 2,
-      y: Math.random() * canvasSize.height - canvasSize.height / 2,
+      x: Math.random() * canvasSize.width,
+      y: -Math.random() * canvasSize.height,
       size: Math.random() * 2 + 1
     };
     stars.push(starData);
   }
 }
 
-export function update(speed) {
-  stars.forEach((star) => {
-    star.y += speed;
-    if (star.y > height / 2) star.y = 0;
+export function update() {
+  stars.forEach(star => {
+    star.y++;
+    if (star.y > height) star.y = 0;
   });
 }
 
 export function render(ctx) {
-  stars.forEach((star) => {
+  stars.forEach(star => {
     ctx.beginPath();
     ctx.arc(star.x, star.y, star.size / 2, 0, Math.PI * 2);
     ctx.fillStyle = '#fff';
